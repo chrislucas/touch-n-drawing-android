ff415e760218cd06719542fd2c6e5ac3d5ad3ae9
chrislucas <christoffer.luccas@gmail.com>
2017-11-06 17:37:19 -0200

    first commit

---
+++ DrawingWithFinger/app/src/main/java/xplore/drawingwithfinger/ActivityCanvas.kt
@@ -0,0 +1,12 @@
+package xplore.drawingwithfinger
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityCanvas : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_canvas)
+    }
+}
